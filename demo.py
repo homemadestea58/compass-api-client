ce5574e77366af9d115033fe1dffcabeb5c32baf
llui85 <47158068+llui85@users.noreply.github.com>
2021-04-20 20:03:25 +1000

    Update demo.py for new config

--- demo.py
+++ demo.py
@@ -1,15 +1,11 @@
 from CompassAPI import CompassAPI
 
 Compass = CompassAPI(
-    prefix = "https://compass-vic.compass.education",
-    oktaPrefix = "https://example.okta.com",
     debug = True
 )
 
-authenticationStatus = Compass.authenticateWithOkta(
-    username = "me",
-    password = "me"
-)
+Compass.loadConfigFromFile()
+authenticationStatus = Compass.authenticate()
 
 if not authenticationStatus:
     print("Authentication error!")
